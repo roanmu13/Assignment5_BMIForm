239fa9b89e94fc182a3942deaae445ac8b582380
Rosa <munguiarosa13@gmail.com>
2017-08-14 18:04:44 -0400

    Added splash form

---
+++ Assignment5_BMIForm/SplashForm.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+/*
+ * Name: Rosa Munguia
+ * Student Number: 300735300
+ * Date: August 14 2017
+ * Description: This is the splash form class for the BMI calculator
+ * Version 0.1- Added splash form
+ */
+namespace Assignment5_BMIForm
+{
+    public partial class SplashForm : Form
+    {
+        public SplashForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
